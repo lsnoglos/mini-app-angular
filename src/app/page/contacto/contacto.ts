7f00ac968d25b2c08bdb6a429a56b9f6d70a6064
noellopez <noel.lopez@mqaamericas.com>
2025-10-26 13:25:17 -0600

    feat: Implement reactive form logic in Contacto

--- src/app/page/contacto/contacto.ts
+++ src/app/page/contacto/contacto.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './contacto.html',
   styleUrl: './contacto.css',
 })
-export class Contacto {
+export class Contacto implements OnInit {
 
+  private fb = inject(FormBuilder); // Inyección de FormBuilder
+
+  contactForm!: FormGroup;
+
+  ngOnInit(): void {
+
+    this.contactForm = this.fb.group({
+      nombreCompleto: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      telefono: ['', [Validators.required, Validators.pattern('/^\d{13}$/')]],
+      mensaje: ['', [Validators.required, Validators.minLength(50)]],
+      aceptarTerminos: [false, [Validators.requiredTrue]]
+    });
+  }
+
+  get f() {
+    return this.contactForm.controls;
+  }
 }

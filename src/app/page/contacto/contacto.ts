32b4e3992ba604daa76331ec8deaf5660aafa6f4
noellopez <noel.lopez@mqaamericas.com>
2025-10-27 00:43:25 -0600

    feat(contacto): Refactor Forms

--- src/app/page/contacto/contacto.ts
+++ src/app/page/contacto/contacto.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
@@ -16,17 +16,24 @@ export class Contacto implements OnInit {
 
   private fb = inject(FormBuilder); // Inyección de FormBuilder
 
-  contactForm!: FormGroup;
+  contactForm!: FormGroup<{
+    nombreCompleto: FormControl<string>;
+    email: FormControl<string>;
+    telefono: FormControl<string>;
+    mensaje: FormControl<string>;
+    aceptarTerminos: FormControl<boolean>;
+  }>;
+
   mensajeExito: string = '';
 
   ngOnInit(): void {
 
     this.contactForm = this.fb.group({
-      nombreCompleto: ['', [Validators.required, Validators.minLength(3)]],
-      email: ['', [Validators.required, Validators.email]],
-      telefono: ['', [Validators.required, Validators.pattern(/^\d{13}$/)]],
-      mensaje: ['', [Validators.required, Validators.minLength(50)]],
-      aceptarTerminos: [false, [Validators.requiredTrue]]
+      nombreCompleto: this.fb.control('', { nonNullable: true, validators: [Validators.required, Validators.minLength(3)] }),
+      email: this.fb.control('', { nonNullable: true, validators: [Validators.required, Validators.email] }),
+      telefono: this.fb.control('', { nonNullable: true, validators: [Validators.pattern(/^(|\d{13})$/)] }),
+      mensaje: this.fb.control('', { nonNullable: true, validators: [Validators.required, Validators.minLength(50)] }),
+      aceptarTerminos: this.fb.control(false, { nonNullable: true, validators: [Validators.requiredTrue] })
     });
   }
 
@@ -34,16 +41,16 @@ export class Contacto implements OnInit {
     return this.contactForm.controls;
   }
 
-  onSubmit(): void { 
+  onSubmit(): void {
     if (this.contactForm.invalid) {
       this.contactForm.markAllAsTouched();
-      this.mensajeExito = ''; 
+      this.mensajeExito = '';
       return;
     }
 
     console.log('Formulario Enviado:', this.contactForm.value);
     this.mensajeExito = '¡Gracias por tu mensaje! Te contactaremos pronto.';
-    
+
     this.contactForm.reset();
   }
 }
